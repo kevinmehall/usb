a7562e2ebd19aef1343c710ad907bafa96eb15a3
Kevin Mehall <km@kevinmehall.net>
2012-07-09 21:44:58 -0600

    usb_pipe: disable interrupts around the pipe-modifying calls for safety.

--- usb_pipe.h
+++ usb_pipe.h
@@ -17,13 +17,19 @@ inline void usb_pipe_handle(uint8_t ep, const Pipe* pipe){
 		if (USB_ep_ready(ep) && pipe_can_read(pipe) > 2){
 			const uint8_t bank = (ep&USB_EP_PP)?pipe->data->read_pos&1:0;
 			USB_ep_start_bank(ep, bank, pipe_read_ptr(pipe), pipe->size);
+
+			cli();
 			pipe_done_read(pipe);
+			sei();
 		}
 	}else{
 		if (USB_ep_ready(ep) && pipe_can_write(pipe) > 2){
 			const uint8_t bank = (ep&USB_EP_PP)?pipe->data->write_pos&1:0;
 			USB_ep_start_bank(ep, bank, pipe_write_ptr(pipe), pipe->size);
+
+			cli();
 			pipe_done_write(pipe);
+			sei();
 		}
 	}
 }
