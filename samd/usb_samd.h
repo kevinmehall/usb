5bd1c5581a1028eb59083b1728df0a416d7be09e
Kevin Mehall <km@kevinmehall.net>
2015-01-14 10:48:52 -0800

    sam0: define USB_ALIGN for alignment attribute

--- samd/usb_samd.h
+++ samd/usb_samd.h
@@ -7,6 +7,8 @@
 extern UsbDeviceDescriptor usb_endpoints[];
 extern const uint8_t usb_num_endpoints;
 
+#define USB_ALIGN __attribute__((__aligned__(4)))
+
 #define USB_ENDPOINTS(NUM_EP) \
 	const uint8_t usb_num_endpoints = (NUM_EP); \
 	UsbDeviceDescriptor usb_endpoints[(NUM_EP)+1];
